5af65be9ecffde7739e8a9a0bd1f4bcfbc1ef58e
Dustin Bleile <dbleile@bcgsc.ca>
2021-03-19 16:44:25 -0700

    SDEV-1736 - remove merge_mafs.R and merge functions into merge_loh.R

--- scripts/merge_loh.R
+++ scripts/merge_loh.R
@@ -13,17 +13,61 @@ library(docopt)
 library(dplyr)
 library(GenomicRanges)
 
-# Use script-relative import replacement for
-#   source("scripts/merge_mafs.R")
-this_folder <- funr::get_script_path()
-source(paste(this_folder, "merge_mafs.R", sep="/"))
-
 args <- docopt(doc)
 print(args)
 if(args$loh == "STDIN"){
   print("Reading from STDIN")
   loh <- read.table(file("stdin"), header=F, sep = "\t", stringsAsFactors = F)
 }else{loh <- read.table(args$loh, header=F, sep="\t", stringsAsFactors = F)}
+
+# merge_mafs.R functions
+merge_mafs <- function(inputmafs, na.rm = T, flip = F, exp = F){
+  if(exp){
+    if(length(na.omit(inputmafs)) == 0){
+      return(NA_character_)
+    }
+    return(paste(na.omit(inputmafs), collapse = ";"))
+  }
+  if(na.rm){
+    inputmafs <- inputmafs[which(!is.na(inputmafs))]
+  }
+  ## Compute magnitude about 0.5
+  mafs_about_zero <- inputmafs - 0.5
+  negatives <- which(mafs_about_zero < 0)
+  positives <- which(mafs_about_zero > 0)
+  ## Compute absolute magnitude about 0.5 and add 0.5
+  flipped_mafs <- abs(mafs_about_zero)
+  if(length(positives) > length(negatives)){
+    out <- mean(0.5 + flipped_mafs)
+  }else{
+    out <- mean(0.5 - flipped_mafs)
+  }
+  if(flip){
+    out <- abs(out - 0.5) + 0.5
+  }
+  return(out)
+}
+unmerge_mafs <- function(merged_mafs, flip = F){
+  mafs <- na.omit(merged_mafs)
+  if(flip){
+    return(abs(as.numeric(unlist(lapply(mafs, strsplit, split = ";"), recursive = T)) - 0.5) + 0.5)
+  }
+  unlist(lapply(mafs, strsplit, split = ";"), recursive = F)
+}
+flip_merged_mafs <- function(merged_mafs){
+  mafs <- unlist(lapply(mafs, strsplit, split = ";"), recursive = F)
+  flipped_mafs <- lapply(mafs, function(x)merge_mafs(abs(as.numeric(x) - 0.5) + 0.5, exp = T))
+  return(flipped_mafs)
+}
+
+unmerge_mafs_grouped <- function(merged_mafs, flip = F){
+  if(flip){
+    mafs <- unlist(lapply(merged_mafs, strsplit, split = ";"), recursive = F)
+    mafs <- lapply(mafs, function(x){median(abs(as.numeric(x) - 0.5) + 0.5)})
+    return(mafs)
+  }
+  unlist(lapply(mafs, strsplit, split = ";"), recursive = F)
+}
 
 windows <- read.table(args$windows, header = F, sep = "\t", stringsAsFactors = F)
 
