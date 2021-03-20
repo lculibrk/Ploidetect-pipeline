0e15284f02e8a6b07903e9938b232757c4dc315a
lculibrk <luculibrk@gmail.com>
2021-03-19 19:15:10 -0700

    Added documentation and comments to scripts and Snakefile

--- scripts/ploidetect_copynumber.R
+++ scripts/ploidetect_copynumber.R
@@ -1,3 +1,8 @@
+# ploidetect_copynumber.R
+# 
+# Runs Ploidetect's copy number caller on Ploidetect-preprocessed input data using Ploidetect's tumor purity/ploidy estimates
+
+# Docopt script docstring
 ' ploidetect_copynumber.R
 
 Usage: 
@@ -11,27 +16,35 @@ Options:
 		--size	maximum iterations to increase resolution [default: Inf]
 
 ' -> doc
-
+#
+# Load packages
 library(docopt)
 library(devtools)
-#devtools::load_all("/projects/lculibrk_prj/CNV/Ploidetect-stable/Ploidetect-package/")
+library(data.table)
 library(Ploidetect)
 library(ggrastr)
-
+#
+# Force data.table to use only one thread (defaults to a large number)
 setDTthreads(1)
-
+#
+# Parse arguments
 args = docopt(doc)
-
-#source("R/ploidetect2.R")
-
+#
+# Read preprocessed input
 in_rds = readRDS(args$input)
+#
+# Read purity/ploidy models
 in_models = read.table(file = args$models, sep = " ", stringsAsFactors=F, header = T)
+#
+# Load centromere positions packaged with Ploidetect
 data(centromeres)
-
+# 
+# Check if Ploidetect couldn't detect purity/ploidy from cnv information and default to 100% purity and diploid
 if(ncol(in_models) == 1){
  in_models = data.frame("tp" = 1, "ploidy" = 2)
 }
-
+#
+# Run ploidetect_cna_sc (subclone aware CNV caller)
 result = ploidetect_cna_sc(all_data = in_rds$all_data, segmented_data = in_rds$segmented_data, tp = in_models$tp[1], ploidy = in_models$ploidy[1], maxpeak=in_rds$maxpeak, verbose = T, max_iters = args$size)
 
 
@@ -107,13 +120,16 @@ for(i in 1:length(CNA_plot)){
 CN_calls <- do.call(rbind.data.frame, CN_calls)
 
 ## End to-do area
-
-
+#
+# Open pdf device
 pdf(args$plots)
+# print plots to send them to the pdf device
 CN_calls
+# Close pdf device
 dev.off()
-
-write.table(file = args$output, x = CN_calls, col.names = T, row.names=F)
+# Write CNV per-bin calls to file
+write.table(file = args$output, x = CN_calls, col.names = T, row.names=F, sep = "\t")
+# Write segmented CNV calls to file
 write.table(file = paste0(gsub(".txt", "", args$output), "_condensed.txt"), x = result$segged_cna_data, col.names = T, row.names = F, quote = F, sep = "\t")
 
 
