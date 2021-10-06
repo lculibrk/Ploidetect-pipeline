lculibrk/Ploidetect-pipeline

commit 736cf1aab0c1496065903339e378aa9a99d252ba

Added check to make sure input bams are all findable

Snakefile
@@ -86,6 +86,11 @@ output_list = []
 for sample in sample_ids:
     somatics = bams_dict[sample]["somatic"].keys()
     normals = bams_dict[sample]["normal"].keys()
+    ## Check that the bams are findable
+    concat_bams = somatics + normals
+    for bam in concat_bams:
+        if not os.path.exists(bam):
+            raise WorkflowSetupError(f"Input file {bam} could not be found. Ensure that the file is spelled correctly, and that you've correctly bound the directory if using singularity")
     combinations = expand("{somatic}_{normal}", somatic=somatics, normal=normals)
     outs = [os.path.join(output_dir, sample, comb, "cna.txt") for comb in combinations]
     output_list.extend(outs)
@@ -137,9 +142,11 @@ rule ploidetect_install:
         "conda_configs/r.yaml"
     params:
         devtools_install(),
+    log:
+        "{output_dir}/logs/ploidetect_install.log"
     shell:
         "export LC_ALL=en_US.UTF-8; "
-        " Rscript -e {params} "
+        " Rscript -e {params} > {log}"
         " && echo {params} > {output} && date >> {output}"
 
 

commit 0e46c64a5a269bcd36be4fce0ca2645041b0b87c

Fixed issues introduced in 736cf1a 2dda004 and 48bd608

Snakefile
@@ -85,9 +85,11 @@ output_list = []
 ## Loop over the bams and construct lib comparisons
 for sample in sample_ids:
     somatics = bams_dict[sample]["somatic"].keys()
+    somatic_paths = bams_dict[sample]["somatic"].values()
     normals = bams_dict[sample]["normal"].keys()
+    normal_paths = bams_dict[sample]["normal"].values()
     ## Check that the bams are findable
-    concat_bams = somatics + normals
+    concat_bams = list(somatic_paths) + list(normal_paths)
     for bam in concat_bams:
         if not os.path.exists(bam):
             raise WorkflowSetupError(f"Input file {bam} could not be found. Ensure that the file is spelled correctly, and that you've correctly bound the directory if using singularity")
@@ -141,12 +143,12 @@ rule ploidetect_install:
     conda:
         "conda_configs/r.yaml"
     params:
-        devtools_install(),
+        install_cmd = devtools_install(),
     log:
-        "{output_dir}/logs/ploidetect_install.log"
+        expand("{output_dir}/logs/ploidetect_install.log", output_dir = output_dir)
     shell:
         "export LC_ALL=en_US.UTF-8; "
-        " Rscript -e {params} > {log}"
+        " Rscript -e {params.install_cmd} > {log}"
         " && echo {params} > {output} && date >> {output}"
 
 