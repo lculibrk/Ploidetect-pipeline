4869ccb3541d764b316a8f594aa4603216910d85
Dustin Bleile <dbleile@bcgsc.ca>
2025-02-19 10:59:38 -0800

    SDEV-4682 - lint - black python style formatting changes.

--- constants.py
+++ constants.py
@@ -1,6 +1,5 @@
-
 VERSION = "v0.3.0"
 
 
 class WorkflowSetupError(ValueError):
-    '''raise this when there's a mistake detected in workflow setup'''
+    """raise this when there's a mistake detected in workflow setup"""
